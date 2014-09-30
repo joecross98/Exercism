8a3631c1373ea4c2caec83e01bda227e0ebe7e72
Joe Cross <joecross@hotmail.com>
2014-09-30 11:12:25 +0200

    Added bob exercise

---
+++ bob/bob.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+
+};
+
+module.exports = Bob;
